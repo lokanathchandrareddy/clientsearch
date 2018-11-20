947a6edecd276d8179a44d5ff3e699ff2ce9be25
lcreddy <lokanath_chandrareddy@student.uml.edu>
2018-11-19 21:15:09 -0500

    populated all the required data

--- src/components/ClientDetail/index.js
+++ src/components/ClientDetail/index.js
@@ -3,7 +3,13 @@ import React from 'react';
 const ClientDetail = (props) => (<div>
     
     <h1> {props.clientProfile.name} </h1>
-    
+    <h1> {props.clientProfile.title} </h1>
+    <h1> {props.clientProfile.quote} </h1>
+    <h1> {props.clientProfile.nationality} </h1>
+    <div>
+        <img src={props.clientProfile.avatar} alt=" avatar" />
+
+    </div>
 </div>
 
 );
